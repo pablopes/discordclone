96a0753d94b1709c563b2d1f33f9260efbe2f976
Pablo Lopes <pablo.infolopes@gmail.com>
2020-07-11 15:29:13 -0300

    finalizando userlist

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import { Avatar, Container, Role, User } from './styles';
 
+interface UserProps {
+    nickname: string;
+    isBot?: boolean;
+}
 
-import { Container} from './styles';
-
-const ServerName: React.FC = () => {
+const UserRow: React.FC<UserProps> = ({ nickname, isBot }) => {
     return (
-       <Container>
-          
-       </Container> 
+        <User>
+            <Avatar className={isBot ? 'bot' : ''}/>
+            <strong>{nickname}</strong>
+            {isBot && <span>Bot</span>}
+        </User>
+
+    );
+};
+const UserList: React.FC = () => {
+    return (
+        <Container>
+            <Role>Disponível - 1</Role>
+            <UserRow nickname="Pablo Lopes" />
+
+            <Role>Offline - 18</Role>
+            <UserRow nickname="Alvo Dumbledore" isBot />
+            <UserRow nickname="Lord Voldemort" isBot />
+            <UserRow nickname="Harry Potter" />
+            <UserRow nickname="Hermione Granger" />
+            <UserRow nickname="Ron Weasley" />
+            <UserRow nickname="Alastor Moody" />
+            <UserRow nickname="Andromeda Tonks" />
+            <UserRow nickname="Bellatrix Lestrange" />
+            <UserRow nickname="Cassandra Trelawney" />
+            <UserRow nickname="Cedric Diggory" />
+            <UserRow nickname="Draco Malfoy" />
+            <UserRow nickname="Lúcio Malfoy" />
+            <UserRow nickname="Rubeus Hagrid" />
+            <UserRow nickname="Severo Snape" />
+            <UserRow nickname="Minerva McGonagall" />
+        </Container>
     )
 };
 
-export default ServerName;
+export default UserList;
